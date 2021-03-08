d8f66ddf1e1b54d8461783542beffd2a014d0c98
kamilpinas <kamilpinas@gmail.com>
2021-03-08 08:57:56 +0100

    L3-TAI

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+let nav = '<nav><ul><li><a href="index.html">Strona główna</a></li>'+
+            '<li><a href="contact.html">Kontakt</a></li>'
+            +'<li><a href="second.html">Nowa Strona</a></li></ul></nav>';
+
+let navbarElem = document.querySelector(".navbar");
+navbarElem.innerHTML = nav;
+
+
